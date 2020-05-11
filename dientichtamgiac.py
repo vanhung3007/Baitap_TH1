b196a2dd73bd43785c208fe9ba140b0b4628c504
vanhung3007 <60846615+vanhung3007@users.noreply.github.com>
2020-05-11 21:54:06 +0700

    Add files via upload

---
+++ dientichtamgiac.py
@@ -0,0 +1,7 @@
+print("Nhập vào độ dài canh đáy tam giác")
+a = int(input())
+print("Nhập vào chiều cao tam giác")
+h = int(input())
+s = float(a * h * 1) / 2
+print("Diện tích tam giác là")
+print(s)
